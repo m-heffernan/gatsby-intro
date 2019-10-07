0b3e43ecb4c5fc77bd309e3dbe7697a0198f40d0
Michael Heffernan <michaelheffernan@Michaels-MacBook-Pro.local>
2019-10-07 19:52:08 -0400

    bunch of changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Helmet from 'react-helmet';
 import Header from '../components/header';
+import useSiteMetadata from '../hooks/use-sitemetadata';
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <>
+      <Global
+        styles={css`
     * {
         box-sizing: border-box
         margin: 0
@@ -50,18 +54,24 @@ const Layout = ({ children }) => (
     }
       }
     `}
-    />
-    <Header></Header>
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 90vw;
-        width: 550px;
-      `}
-    >
-      {children}
-    </main>
-  </>
-);
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
 
 export default Layout;
