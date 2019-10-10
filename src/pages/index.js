0facab296b5473e39e6fce0e91e1dd296d70de8e
Michael Heffernan <michaelheffernan@Michaels-MacBook-Pro.local>
2019-10-09 20:43:57 -0400

    Add images with gatsby relevant plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
 
@@ -8,15 +8,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Astoria!</p>
-      <Link to="/about/">Learn about me &rarr;</Link>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
